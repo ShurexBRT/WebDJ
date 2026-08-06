6f1efb8ea7cad9dd86d8a09af412652802f22989
Pedja Jovanovic <58230377+ShurexBRT@users.noreply.github.com>
2026-08-06 08:47:12 +0200

    Register the WebDJ offline worker

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import { registerWebDjServiceWorker } from './pwa/registerServiceWorker'
 import './styles.css'
 import './render-overrides.css'
+
+void registerWebDjServiceWorker()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
